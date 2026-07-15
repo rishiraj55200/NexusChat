9d89b8a6d0f8b18e4d3b7f27cd2fc36380470ace
rishiraj55200 <rishiraj132000@gmail.com>
2026-07-15 21:01:40 +0530

    change index file for deploy

--- backend/user/src/index.ts
+++ backend/user/src/index.ts
@@ -29,7 +29,7 @@ app.use(cors());
 
 app.use("/api/v1", userRoutes);
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
